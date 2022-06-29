f9d5d32fd3584290f598b4fcfecada1f73e08d62
Jack Gisby <jackgisby@gmail.com>
2022-06-29 21:37:16 +0100

    Add airflow entrypoint

---
+++ airflow/scripts/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Create environment variables for accessing GCS 
+export GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS}
+export AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT=${AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT}
+
+airflow db upgrade
+
+# Create airflow user
+airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
+# "$_AIRFLOW_WWW_USER_USERNAME" -p "$_AIRFLOW_WWW_USER_PASSWORD"
+
+# Launch webserver
+airflow webserver
