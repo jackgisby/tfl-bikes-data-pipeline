aa48c9e2045ebc8a00f60e6f6b0dc331496933da
Jack Gisby <jackgisby@gmail.com>
2022-06-30 12:45:01 +0100

    Improve logging

--- airflow/dags/ingest_bike_data.py
+++ airflow/dags/ingest_bike_data.py
@@ -18,7 +18,7 @@ BIGQUERY_DATASET = environ.get("BIGQUERY_DATASET", "bikes_data_warehouse")
 AIRFLOW_HOME = environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 
-def get_usage_filenames_from_time(execution_date):
+def get_usage_file_names_from_time(execution_date):
     """
     Each (weekly) usage data file in the source bucket has a complex naming pattern including:
         - An ID incrementing with each week
@@ -35,23 +35,28 @@ def get_usage_filenames_from_time(execution_date):
 
     # 2017 IDs start at 38 - this is added to the number of weeks since the first data chunk
     dataset_id = 38 + ((execution_date - datetime(2017, 1, 3, 20, tzinfo=execution_date.tzinfo)).days // 7)
+    logging.info(f"The dataset's ID is {dataset_id}")
 
     # Get the start and end ranges of the data (execution_date refers to the end of the data range)
     dataset_start_range = (execution_date - timedelta(days=6)).strftime("%d%b%Y")
     dataset_end_range = execution_date.strftime("%d%b%Y")
+    logging.info(f"Will extract data for the time period {dataset_start_range} to {dataset_end_range}")
 
     # Get the properly formatted dataset name
-    return f"{dataset_id}JourneyDataExtract{dataset_start_range}-{dataset_end_range}.csv"
+    formatted_dataset_name = f"{dataset_id}JourneyDataExtract{dataset_start_range}-{dataset_end_range}.csv"
+    logging.info(f"Formatted dataset name: {formatted_dataset_name}")
 
-def format_to_parquet(csv_filename):
+    return formatted_dataset_name
+
+def format_to_parquet(csv_file_name):
     """
     Converts a CSV file to a parquet file. Parquet files are ideal because they allow for
     efficient data compression while allowing queries to read only the necessary columns.
 
-    :param csv_filename: The CSV file to be converted.
+    :param csv_file_name: The CSV file to be converted.
     """
 
-    if not csv_filename.lower().endswith(".csv"):
+    if not csv_file_name.lower().endswith(".csv"):
         raise TypeError("Can only convert CSV files to parquet")
 
     # Import pyarrow within task
@@ -59,16 +64,22 @@ def format_to_parquet(csv_filename):
     from pyarrow.parquet import write_table
 
     # Convert CSV to parquet
-    write_table(read_csv(csv_filename), csv_filename.replace(".csv", ".parquet"))
+    parquet_file_name = csv_file_name.replace(".csv", ".parquet")
+    logging.info(f"Parquetised dataset name: {parquet_file_name}")
 
-def reformat_locations_xml(xml_filename):
+    write_table(read_csv(csv_file_name), parquet_file_name)
+
+    return parquet_file_name
+
+
+def reformat_locations_xml(xml_file_name):
     """
     Converts a live XML corresponding to bike locations and extracts relevant data fields.
 
-    :param xml_filename: The name of the XML file to be processed.  
+    :param xml_file_name: The name of the XML file to be processed.  
     """
 
-    if not xml_filename.lower().endswith(".xml"):
+    if not xml_file_name.lower().endswith(".xml"):
         raise TypeError("Can only extract data from XML files")
 
     # Modules for converting xml to csv
@@ -76,22 +87,36 @@ def reformat_locations_xml(xml_filename):
     from xml.etree import ElementTree
 
     # Get the root of the XML
-    locations_tree = ElementTree.parse(xml_filename)
+    locations_tree = ElementTree.parse(xml_file_name)
     stations = locations_tree.getroot()
 
     # These are the variables we wish to extract
     station_vars = ["id", "name", "terminalName", "lat", "long"]
 
     # Create replacement CSV
-    with open(xml_filename.replace(".xml", ".csv"), "w") as output_file:
+    csv_file_name = xml_file_name.replace(".xml", ".csv")
+    logging.info(f"CSV dataset name: {csv_file_name}")
+
+    with open(csv_file_name, "w") as output_file:
 
         # Write header to CSV
         output_csv = csv.writer(output_file)
         output_csv.writerow(station_vars)
         
         # Loop through each node (station) and find each variable, write to CSV
-        for station in stations:
-            output_csv.writerow([station.find(station_var).text for station_var in station_vars])
+        logging.info("Preview of XML to CSV conversion:")
+        logging.info(station_vars)
+
+        for i, station in enumerate(stations):
+
+            csv_row = [station.find(station_var).text for station_var in station_vars]
+
+            if i < 5:
+                logging.info(csv_row)
+
+            output_csv.writerow(csv_row)
+
+    return csv_file_name
 
 
 with DAG(
@@ -110,14 +135,14 @@ with DAG(
 ) as ingest_bike_usage:
 
     # Get the name of the file using the date of the DAG run
-    get_filenames = PythonOperator(
-        task_id = "get_filenames",
-        python_callable = get_usage_filenames_from_time
+    get_file_names = PythonOperator(
+        task_id = "get_file_names",
+        python_callable = get_usage_file_names_from_time
     )
 
-    # Get the return value of the get_filenames task
-    dataset_name = "{{ ti.xcom_pull(task_ids='get_filenames') }}"
-    logging.debug(f"dataset_name: {dataset_name}")
+    # Get the return value of the get_file_names task
+    dataset_name = "{{ ti.xcom_pull(task_ids='get_file_names') }}"
+    logging.info(f"Pulled csv name: {dataset_name}")
 
     # The week"s data is downloaded to the local machine
     download_file_from_https = BashOperator(
@@ -130,11 +155,13 @@ with DAG(
         task_id = "convert_to_parquet",
         python_callable = format_to_parquet,
         op_kwargs = {
-            "csv_filename": f"{AIRFLOW_HOME}/{dataset_name}",
+            "csv_file_name": f"{AIRFLOW_HOME}/{dataset_name}",
         },
     )
 
-    dataset_name = dataset_name.replace(".csv", ".parquet")
+    # Get the new dataset_name after conversion to parquet
+    dataset_name = "{{ ti.xcom_pull(task_ids='convert_to_parquet') }}"
+    logging.info(f"Pulled parquet name: {dataset_name}")
 
     # The local data is transferred to the GCS 
     transfer_data_to_gcs = LocalFilesystemToGCSOperator(
@@ -144,7 +171,7 @@ with DAG(
         bucket = GCP_GCS_BUCKET
     )
     
-    get_filenames >> download_file_from_https >> convert_to_parquet >> transfer_data_to_gcs
+    get_file_names >> download_file_from_https >> convert_to_parquet >> transfer_data_to_gcs
 
 
 with DAG(
@@ -175,18 +202,18 @@ with DAG(
         task_id = "convert_from_xml",
         python_callable = reformat_locations_xml,
         op_kwargs = {
-            "xml_filename": f"{AIRFLOW_HOME}/{dataset_name}",
+            "xml_file_name": f"{AIRFLOW_HOME}/{dataset_name}",
         },
     )
 
     dataset_name = dataset_name.replace(".xml", ".csv")
-    
+
     # We convert to the columnar parquet format for upload to GCS
     convert_to_parquet = PythonOperator(
         task_id = "convert_to_parquet",
         python_callable = format_to_parquet,
         op_kwargs = {
-            "csv_filename": f"{AIRFLOW_HOME}/{dataset_name}",
+            "csv_file_name": f"{AIRFLOW_HOME}/{dataset_name}",
         },
     )
 
